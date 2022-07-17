805eb387136337f7f7e71b36817bbbc3edc28d32
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-17 07:39:14 +0200

    milestone 2 completed, added comments and reordered for readability

--- js/script.js
+++ js/script.js
@@ -6,12 +6,19 @@ Vi ricordo che i ticket devono essere inerenti alla milestone del giorno e se vo
 Di seguito riportate tutte le milestone obbligatorie, domani parleremo della milestone opzionale (bonus):
 Milestone 1 (DAY 1)
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto */
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo. */
 
 const root = new Vue({
     name: 'boolzapp',
     el: '#root',
-    data: {
+    data: 
+      {
         user: {
         name: 'Nome Utente',
         avatar: '_io'
@@ -96,9 +103,14 @@ const root = new Vue({
           }
           ],
         },
-      ]
+      ],
+      contactIndex: 0
     },
-  methods: {
-      }
-
+    methods: {
+        selectContact(i){
+          this.contacts.forEach((contact, index) => {
+            if(index === i) return this.contactIndex = i
+          });
+        }
+      },
 })
